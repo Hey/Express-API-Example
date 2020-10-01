83f0752cb14110ff4dbb6185d7a29b00364d0919
Chazza <ch@rli.es>
2020-10-01 21:01:22 +0100

    Override changes

--- routes/Front.js
+++ routes/Front.js
@@ -3,11 +3,10 @@ const { Router } = require('express')
 const route = Router()
 
 // Get the controller for this endpoint.
-const Front = require('../controllers/Front.js')
+const Front = require('../controllers/front.js')
 
 // Endpoint defining.
 route.get('/', Front.index)
 route.get('/git', Front.git)
-route.get('/fetch', Front.fetch)
 
 module.exports = route
