07212b3989f744312b9f0c194cbe3479ab14dab6
Hey <18427051+Hey@users.noreply.github.com>
2020-10-03 09:51:10 +0200

    1 line the console log when web server is ready.

--- index.js
+++ index.js
@@ -25,6 +25,4 @@ app.use(middlewares.errorHandler)
 app.use(middlewares.notFound)
 
 // Run server.
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Listening: http://localhost:${process.env.PORT || 8080}`)
-})
+app.listen(process.env.PORT || 8080, () => console.log(`Listening: http://localhost:${process.env.PORT || 8080}`))
